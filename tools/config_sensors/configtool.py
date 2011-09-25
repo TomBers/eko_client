8180aa38cd5312eae11ab1a75b116cb6790b99d6
Charith Amarasinghe <charith.amarasinghe@gmail.com>
2011-09-25 14:52:21 +0100

    Changed file modes of executables to 755

--- tools/config_sensors/configtool.py
+++ tools/config_sensors/configtool.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python
-import optparse
 import logging
 import sys
 import time
-import signal
-import socket
-import urllib2
 
 from ConfigParser import ConfigParser
-
-from eko.SystemInterface.DisplayControl import DisplayController
 
 import eko.Util.LogHelper as LogHelper
 import eko.Util.DBSetup as DBSetup
@@ -24,7 +18,6 @@ import eko.SystemInterface.OSTools as OSTools
 
 import eko.SystemInterface.Beagleboard as Beagleboard
 
-from eko.ThirdParty import ping
 
 logger = LogHelper.getLoggerInstance()
 logger.setLevel(logging.DEBUG)
